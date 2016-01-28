245d8d48cc5c3e17c4609611d14ed2cc8422fc9d
yours3lf <nekem28@msn.com>
2016-01-28 15:21:07 +0100

    fixed

--- mipmap.cpp
+++ mipmap.cpp
@@ -5,18 +5,18 @@ using namespace prototyper;
 void set_workgroup_size( vec2& gws, vec2& lws, vec2& dispatch_size, const uvec2& screen )
 {
   //set up work group sizes
-  unsigned local_ws[2] = {16, 16};
+  unsigned local_ws[2] = { 16, 16 };
   unsigned global_ws[2];
   unsigned gw = 0, gh = 0, count = 1;
-  
+
   while( gw < screen.x )
   {
     gw = local_ws[0] * count;
     count++;
   }
-  
+
   count = 1;
-  
+
   while( gh < screen.y )
   {
     gh = local_ws[1] * count;
@@ -31,7 +31,7 @@ void set_workgroup_size( vec2& gws, vec2& lws, vec2& dispatch_size, const uvec2&
   dispatch_size = gws / lws;
 }
 
-void gen_mipmaps( GLuint shader, GLuint texture, GLenum internal_format, const uvec2& size, unsigned miplevels)
+void gen_mipmaps( GLuint shader, GLuint texture, GLenum internal_format, const uvec2& size, unsigned miplevels )
 {
   glUseProgram( shader );
 
@@ -42,7 +42,7 @@ void gen_mipmaps( GLuint shader, GLuint texture, GLenum internal_format, const u
 
   for( int d = 1; d < miplevels; ++d )
   {
-    glBindImageTexture( 0, texture, d-1, GL_FALSE, 0, GL_READ_ONLY, internal_format );
+    glBindImageTexture( 0, texture, d - 1, GL_FALSE, 0, GL_READ_ONLY, internal_format );
     glBindImageTexture( 1, texture, d, GL_FALSE, 0, GL_WRITE_ONLY, internal_format );
 
     vec2 dispatch_size, gws, lws;
@@ -70,7 +70,7 @@ int main( int argc, char** argv )
 
   framework frm;
   frm.init( screen, title, fullscreen );
-  frm.set_vsync( true );
+  //frm.set_vsync( true );
 
   //set opengl settings
   glEnable( GL_DEPTH_TEST );
@@ -99,7 +99,7 @@ int main( int argc, char** argv )
 
   /**/
 
-  GLuint ss_quad = frm.create_quad( vec3(-1, -1, 0), vec3(1, -1, 0), vec3(-1, 1, 0), vec3(1, 1, 0) );
+  GLuint ss_quad = frm.create_quad( vec3( -1, -1, 0 ), vec3( 1, -1, 0 ), vec3( -1, 1, 0 ), vec3( 1, 1, 0 ) );
 
   sf::Image im;
   im.loadFromFile( "../resources/image.jpg" );
@@ -113,22 +113,26 @@ int main( int argc, char** argv )
   glGenTextures( 1, &tex );
 
   glBindTexture( GL_TEXTURE_2D, tex );
-  
-  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
-  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
-  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
-  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 4);
-  
-  glTexStorage2D( GL_TEXTURE_2D, miplevels, internal_format, size.x, size.y );
 
-  glTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, size.x, size.y, GL_RGBA, GL_UNSIGNED_BYTE, im.getPixelsPtr() );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+  //glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
+  //glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+  //glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 4 );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
 
-  //glGenerateMipmap( GL_TEXTURE_2D );
-  gen_mipmaps( downsample_shader, tex, internal_format, size, miplevels ); 
+  //glTexStorage2D( GL_TEXTURE_2D, miplevels, internal_format, size.x, size.y );
+  //glTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, size.x, size.y, GL_RGBA, GL_UNSIGNED_BYTE, im.getPixelsPtr() );
+  glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA8, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, im.getPixelsPtr() );
+
+  glHint( GL_GENERATE_MIPMAP_HINT, GL_NICEST );
+
+  glGenerateMipmap( GL_TEXTURE_2D );
+  //gen_mipmaps( downsample_shader, tex, internal_format, size, miplevels ); 
 
   //create texview
-  GLuint texview = 0;
+  /*GLuint texview = 0;
   glGenTextures( 1, &texview );
 
   glTextureView( texview, GL_TEXTURE_2D, tex, GL_RGBA8, 0, miplevels, 0, 1 );
@@ -137,7 +141,7 @@ int main( int argc, char** argv )
   glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
   glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
   glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 4);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 4);*/
 
   /**/
 
@@ -147,28 +151,35 @@ int main( int argc, char** argv )
 
   float lod = 0;
 
+  bool default_or_mine = false; //default
+
   auto event_handler = [&]( const sf::Event & ev )
   {
     switch( ev.type )
     {
       case sf::Event::KeyPressed:
-        {
-          if( ev.key.code == sf::Keyboard::Add )
-          {
-            if( lod < 9 )
-            {
-              lod += 0.1f;
-            }
-          }
+      {
+                                  if( ev.key.code == sf::Keyboard::Add )
+                                  {
+                                    if( lod < 10 )
+                                    {
+                                      lod += 0.1f;
+                                    }
+                                  }
 
-          if( ev.key.code == sf::Keyboard::Subtract )
-          {
-            if( lod > 0 )
-            {
-              lod -= 0.1f;
-            }
-          }
-        }
+                                  if( ev.key.code == sf::Keyboard::Subtract )
+                                  {
+                                    if( lod > 0 )
+                                    {
+                                      lod -= 0.1f;
+                                    }
+                                  }
+
+                                  if( ev.key.code == sf::Keyboard::Space )
+                                  {
+                                    default_or_mine = !default_or_mine;
+                                  }
+      }
       default:
         break;
     }
@@ -193,13 +204,24 @@ int main( int argc, char** argv )
 
     glViewport( 0, 0, screen.x, screen.y );
 
+    if( default_or_mine )
+    {
+      gen_mipmaps( downsample_shader, tex, internal_format, size, miplevels );
+      frm.set_title( "mine " + std::to_string( int( lod ) + 1 ) );
+    }
+    else
+    {
+      glGenerateMipmap( GL_TEXTURE_2D );
+      frm.set_title( "default " + std::to_string( int( lod ) + 1 ) );
+    }
+
     glUseProgram( display_shader );
 
     glUniform1f( 0, lod );
 
     glActiveTexture( GL_TEXTURE0 );
-    glBindTexture( GL_TEXTURE_2D, texview );
-    //glBindTexture( GL_TEXTURE_2D, tex );
+    //glBindTexture( GL_TEXTURE_2D, texview );
+    glBindTexture( GL_TEXTURE_2D, tex );
 
     glBindVertexArray( ss_quad );
     glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
--- shaders/mipmap/downsample.cs
+++ shaders/mipmap/downsample.cs
@@ -5,98 +5,23 @@ layout(binding=1) writeonly uniform image2D dst_tex;
 
 layout(local_size_x = 16, local_size_y = 16) in; //local workgroup size
 
-vec4 sample_point( vec2 coord, vec2 size )
+vec4 sample_box( vec2 coord, vec2 src_size, vec2 dst_size )
 {
-  return imageLoad( src_tex, ivec2(coord * size) );
-}
+  ivec2 bottom_left_pixel = ivec2(coord * src_size);
 
-vec4 sample_bilinear( vec2 coord, vec2 size )
-{
-  ivec2 final_coord = ivec2(coord * size);
-  vec4 s00 = imageLoad( src_tex, final_coord + ivec2(0, 0) );
-  vec4 s01 = imageLoad( src_tex, final_coord + ivec2(1, 0) );
-  vec4 s10 = imageLoad( src_tex, final_coord + ivec2(0, 1) );
-  vec4 s11 = imageLoad( src_tex, final_coord + ivec2(1, 1) );
-  
-  float xval = fract( coord.x * size.x );
-  float yval = fract( coord.y * size.y );
-  
-  return mix( mix( s00, s10, xval ), mix( s01, s11, xval ), yval );
-}
+  float sample_count = 0;
+  vec4 sample_sum = vec4(0);
 
-float triangular( float f )
-{
-  f *= 0.5;
-  return f < 0 ? f + 1 : 1 - f;
-}
+  ivec2 kernel_size = ivec2(src_size / dst_size);
 
-float bell( float f )
-{
-  f = f * (0.5 * 1.5); //rescale [-2...2] to [-1.5...1.5]
-  
-  if( f >= -1.5 && f < -0.5 )
-  {
-    f += 1.5;
-    return 0.5 * f * f;
-  }
-  else if( f >= -0.5 && f < 0.5 )
-  {
-    return 0.75 - ( f * f );
-  }
-  else if( f >= 0.5 && f < 1.5 )
-  {
-    f -= 1.5;
-    return 0.5 * f * f;
-  }
-  else
-  {
-    return 0;
-  }
-}
-
-float bspline( float f )
-{
-  f = abs(f);
-  
-  if( f >= 0 && f <= 1 )
-  {
-    return (2/3.0) + 0.5 * ( f * f * f ) - ( f * f );
-  }
-  else if( f > 1 && f <= 2 )
-  {
-    f = 2 - f;
-    return (1/6.0) * f * f * f;
-  }
-  else
-  {
-    return 1;
-  }
-}
-
-vec4 sample_bicubic( vec2 coord, vec2 size )
-{
-  vec4 sum = vec4(0);
-  vec4 weight_sum = vec4(0);
-  
-  ivec2 final_coord = ivec2(coord * size);
-  
-  float xval = fract( coord.x * size.x );
-  float yval = fract( coord.y * size.y );
-  
-  for( int y = -1; y <= 2; ++y )
-  {
-    for( int x = -1; x <= 2; ++x )
+  for(int y = 0; y < kernel_size.y; ++y)
+    for(int x = 0; x < kernel_size.x; ++x)
     {
-      vec4 s = imageLoad( src_tex, final_coord + ivec2(x, y) );
-      float fx = bspline( x - xval ); //can use bell or triangular as functions
-      float fy = bspline( -(y - yval) );
-      float weight = fx * fy;
-      sum += s * weight;
-      weight_sum += weight;
+      sample_sum += imageLoad(src_tex, bottom_left_pixel + ivec2(x,y));
+      sample_count++;
     }
-  }
-  
-  return sum / weight_sum;
+
+  return sample_sum / sample_count;
 }
 
 void main()
@@ -105,16 +30,10 @@ void main()
   ivec2 global_size = imageSize( dst_tex ).xy;
   ivec2 src_size = imageSize( src_tex ).xy;
   vec2 texcoord = vec2(global_id) / vec2(global_size);
-  
+
   if( global_id.x <= global_size.x && global_id.y <= global_size.y )
-	{  
-    //point filtering
-    //imageStore( dst_tex, global_id, sample_point( texcoord, src_size ) );
-    
-    //bilinear filtering
-    //imageStore( dst_tex, global_id, sample_bilinear( texcoord, src_size ) );
-    
-    //bicubic filtering
-    imageStore( dst_tex, global_id, sample_bicubic( texcoord, src_size ) );
+	{
+    //box filter sampling
+    imageStore( dst_tex, global_id, sample_box( texcoord, src_size, global_size ) );
   }
 }
